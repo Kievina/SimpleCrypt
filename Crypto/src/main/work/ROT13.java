71032497483207bf0d663ad7a2e7339787fc7ae7
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-18 13:26:49 -0500

    rotate tests pass

--- Crypto/src/main/work/ROT13.java
+++ Crypto/src/main/work/ROT13.java
@@ -1,17 +1,20 @@
+package work;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
 
     ROT13(Character cs, Character cf) {
     }
 
     ROT13() {
+        this('a', 'm');
     }
 
-
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder builder = new StringBuilder();
 
         return "";
     }
@@ -25,8 +28,6 @@ public class ROT13  {
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        return s.substring(s.indexOf(c)) + s.substring(0,s.indexOf(c));
     }
-
 }
--- Crypto/src/test/work/ROT13Test.java
+++ Crypto/src/test/work/ROT13Test.java
@@ -1,3 +1,5 @@
+package work;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
