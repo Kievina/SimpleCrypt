726ec71cba7f4a3abef1503c9bb6ae9a575ebee6
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-21 23:54:54 -0500

    all original tests now pass

--- Crypto/src/main/work/ROT13.java
+++ Crypto/src/main/work/ROT13.java
@@ -1,12 +1,5 @@
 package work;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import static java.lang.Character.isLowerCase;
-import static java.lang.Character.isUpperCase;
-import static java.lang.Character.toLowerCase;
-
 public class ROT13 {
     private Integer offset;
 
@@ -20,55 +13,33 @@ public class ROT13 {
     }
 
     public String crypt(String text) throws UnsupportedOperationException {
-
         return text;
     }
 
     public String encrypt(String text) {
         StringBuilder builder = new StringBuilder();
-        String alpha = "abcdefghijklmnopqrstuvw";
+        int keyIdx;
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
         String rotatedAlpha = rotate(alpha, 'n');
         for (int i = 0; i < text.length(); i++) {
             if (Character.isLetter(text.charAt(i))) {
-                if (Character.isUpperCase(text.charAt(i)))
-                    builder.append(Character.toUpperCase(rotatedAlpha.charAt(i)));
-                else
-                    builder.append(rotatedAlpha.charAt(i));
+                if (Character.isUpperCase(text.charAt(i))) {
+                    keyIdx = alpha.indexOf(Character.toLowerCase(text.charAt(i)));
+                    builder.append(Character.toUpperCase(rotatedAlpha.charAt(keyIdx)));
+                } else {
+                    keyIdx = alpha.indexOf(text.charAt(i));
+                    builder.append(rotatedAlpha.charAt(keyIdx));
+                }
             } else {
                 builder.append(text.charAt(i));
             }
         }
         return builder.toString();
-//        StringBuilder builder = new StringBuilder();
-//        for (char c : text.toCharArray()) {
-//            if (Character.isLetter(c)) {
-//                if (Character.isUpperCase(c)) {
-//                    c += offset;
-//                    if (c > 'Z') {
-//                        c -= 26;
-//                        builder.append(c);
-//                    }
-//                } else {
-//                    c += offset;
-//                    if (c > 'z') {
-//                        c -= 26;
-//                        builder.append(c);
-//                    }
-//                }
-//            } else {
-//                builder.append(c);
-//            }
-//        }
-//        return builder.toString();
     }
 
 
     public String decrypt(String text) {
-        Integer hold = offset;
-        offset = 26 - offset;
-        String result = encrypt(text);
-        offset = hold;
-        return result;
+        return encrypt(text);
     }
 
     public static String rotate(String s, Character c) {
